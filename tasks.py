db61308db601703d09291f9e92c2f00a096ed331
rsahtasheva24-hue <r.sahtasheva24@apec.edu.kz>
2026-04-02 11:44:24 +0500

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@
 # n = int(input())
 
 # for i in range(1, n + 1):
-#     if (i % 3 == 0 or i % 5 == 0) and not (i % 3 == 0 or i % 5 == 0):
+#     if i % 3 == 0 or i % 5 == 0:
 #         print(i, end="")
 
 # task 2
